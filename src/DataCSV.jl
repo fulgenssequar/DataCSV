08c08f5bc45378bec4ecbb8ce46e09ec5cc5d92a
= <=>
2020-08-25 12:52:17 +0800

    Direct append to csv file after interruption

--- src/DataCSV.jl
+++ src/DataCSV.jl
@@ -2,24 +2,26 @@ module DataCSV
 # Use CSV files to store huge data
 # Where colomn :Data stores large data as Strings, while all other columns store keys.
 
-export CSVInfo, dict2Row, row2File, dict2File, file2Rows,  findRows, keyExists, file2Keys, headFirst, iterForward
+export CSVInfo, dict2Row, row2File, dict2File, file2Rows,  findRows, keyExists, file2Keys, headFirst, iterForward, iterFromLast, SymbolRange
 
 using DataFrames
 using CSV
 
 struct CSVInfo
-    keys::Set{Symbol}
+    keys::Array{Symbol}
     keytypes::Dict{Symbol, Type}
     fileName::String
 end
 
-function CSVInfo(sample::Union{Dict, NamedTuple}, fileName::String)
-    if (:Data in keys(sample))
+SymbolRange = Dict{Symbol, Union{Function, <: AbstractArray}}
+
+function CSVInfo(sample::Union{Dict, NamedTuple}, fileName::String; skeys = collect(keys(sample)))
+    if (:Data in skeys)
         println("Warning: Column named :Data is occupied. Get another name.")
     end
-    keytypes = Dict([k => typeof(sample[k]) for k in keys(sample)])
+    keytypes = Dict([k => typeof(sample[k]) for k in skeys])
     CSVInfo(
-        Set(keys(sample)),
+        skeys,
         merge(keytypes, Dict(:Data=> String)),
         fileName
     )
@@ -71,7 +73,7 @@ function goodRow(key::Union{Dict, NamedTuple}, row)
         return true
 end
 
-function useRow(r; nokeys = false, dmapper= identity)
+function useRow(r; nokeys = true, dmapper= identity)
     data=r[:Data]
     try
         data = eval(Meta.parse(r[:Data]))
@@ -114,22 +116,22 @@ trueRange(rg::Function, keyMap::Dict) = begin
     rg(keyMap)
 end
 
-function rangesFirst(keys::AbstractArray, ranges::Dict; n::Int = 1, keyMap = Dict())
+function rangesFirst(keys::AbstractArray{Symbol}, ranges::SymbolRange; n::Int = 1, keyMap = Dict())
     if (n > length(keys))
         return ranges
     end
     k = keys[n]
     rg  = trueRange(ranges[k], keyMap)
     if (isempty(rg))
-         Dict()
+         Dict( )
     else
         keyMap1 = merge(keyMap, Dict(k => rg[1]))
-        ranges1 = merge(ranges, Dict( k => rg ))
+        ranges1::Dict = merge(ranges, SymbolRange( k => rg ))
         rangesFirst(keys, ranges1; n = n + 1, keyMap = keyMap1)
     end
 end
 
-function headFirst(keys::Array, ranges::Dict, f::Function; n::Int = 1, pace::Int = -1)::Dict
+function headFirst(keys::AbstractArray, ranges::SymbolRange, f::Function; n::Int = 1, pace::Int = -1)::Dict
     # Find the unique point (p) in a sequence where f (p)  turns from true to false,
     # where the sequence is ordered by iterating and combining the items from  each range
     # and returns the ranges each of which contains the uniterated elements in the local loop.
@@ -144,7 +146,7 @@ function headFirst(keys::Array, ranges::Dict, f::Function; n::Int = 1, pace::Int
     
     k = keys[n]
     rg = ranges2[k]
-    ranges1 = merge(ranges, Dict(k => rg))
+    ranges1 = merge(ranges, SymbolRange(k => rg))
     if (length(rg) <= 1)
         return headFirst( keys, ranges1, f; n = n + 1)
     end
@@ -158,20 +160,26 @@ function headFirst(keys::Array, ranges::Dict, f::Function; n::Int = 1, pace::Int
         ranges1[k] = rg[1 + paceGood : end]
         return headFirst( keys, ranges1, f; n = n )
     elseif paceGood > 1
+        #between rg[1] and rg[pace + 1]
         headFirst( keys, ranges1, f; pace = div(paceGood, 2), n = n  )
-    else
-        # between rg and rg.tail
+    else n < length(keys)
+        # between rg[1] and rg.[2]
         out = headFirst( keys, ranges1, f; n = n + 1)
-        if (isempty(out))
-            ranges1[k] = rg[2 : end]
-            return rangesFirst(keys, ranges1)
+        if (isempty(out) )
+            if (n < length(keys))
+                println("Sigular Situation $k: ranges1=$ranges1")
+                ranges1[k] = rg[2 : end]
+                return rangesFirst(keys, ranges1)
+            else
+                return ranges1
+            end
         else
            return out
         end
     end
 end
 
-function iterFromInit(f::Function, keys::AbstractArray, ranges::Dict; init = ranges) 
+function iterFromInit(f::Function, keys::AbstractArray, ranges::SymbolRange; init = ranges) 
     function worker(ks::AbstractArray, paras::Dict  = Dict(); useInit::Bool = true)
         if (isempty(ks))
             f(paras)
@@ -179,7 +187,7 @@ function iterFromInit(f::Function, keys::AbstractArray, ranges::Dict; init = ran
         end
         k = ks[1]
         if useInit
-            rg = init[k]
+            rg = trueRange(init[k], paras)
             for r in rg[1 : 1]
                 paras1 = merge(paras, Dict(k => r))
                 worker(ks[2 : end], paras1; useInit = true)
@@ -211,7 +219,7 @@ function getRapidChecker(info::CSVInfo)::Function
     f
 end
 
-function iterForward(f::Function, iterKeys::AbstractArray, iterRanges::Dict, info::CSVInfo; keyForData = (p, d) -> p)
+function iterForward(f::Function, iterKeys::AbstractArray, iterRanges::SymbolRange , info::CSVInfo; keyForData = (p, d) -> p)
     # Do new iteration or resume the interrupted csv file.
     function runAndSave(paras)
         data = f( paras )
@@ -230,6 +238,82 @@ function getKeyChecker(info)
         println("Finished reading the data file!, $(length(keysInFile)) Items")
     end
     k -> keyExists(k, keysInFile)
+end
+
+function readLastLine(info::CSVInfo)
+    iName = info.fileName
+    oName = ".temp_history.csv"
+    run( pipeline(`head -n 1 $iName`, oName) )
+    open( io -> run( pipeline(`tail -n 1 $iName`, stdout = io)), oName, "a" )
+    lastRow = collect(CSV.Rows(oName; types = info.keytypes ))[end]
+    res = Dict([ k => lastRow[k] for k in keys(lastRow) ])
+    rm(oName)
+    res
+end
+
+function getLastKey(iKeys, iRanges, oldOut; n = 1, init = Dict())::SymbolRange
+    # Get the final iteration list where the last csv file dies out
+    k = iKeys[n]
+    target = oldOut[k]
+    rg = trueRange( iRanges[k], oldOut )
+
+    dealTarget(l, t) =
+        if length( l ) <= 1
+            l
+        elseif l[ 1 ] == t
+            l
+        else
+            dealTarget( l[ 2 : end ], t )
+        end
+    
+    rg1 = dealTarget(rg, target)
+    init[k] = rg1
+    if n < length(iKeys)
+        getLastKey(iKeys, iRanges, oldOut; n = n + 1, init = init)
+    else
+        init
+    end
+end
+
+function getNextKey(iKeys::AbstractArray, iRanges::SymbolRange, tRanges::SymbolRange; n = 1, status = false) 
+    if ( status )
+        return (iRanges, true)
+    elseif n > length( iKeys )
+        return (iRanges, false)
+    else
+        k = iKeys[ n ] 
+        rg = tRanges[ k ]
+        iRanges1 = merge( iRanges, SymbolRange( k => rg ) )
+        res, st1= getNextKey( iKeys, iRanges1, tRanges; n = n + 1, status = false )
+        if ( st1 )
+            return res
+        else
+            if length( rg ) > 1
+                iRanges[ k ] = rg[ 2 : end ]
+                return ( rangesFirst( iKeys, iRanges ), true )
+            else
+                return ( iRanges, false )
+            end
+        end
+    end
+end
+
+function iterFromLast(f::Function, iKeys::AbstractArray, iRanges::SymbolRange, info::CSVInfo; init = iRanges, keyForData = (p, d) -> p)
+    # If iteration has history, resume from the csv file in storage; otherwise do normal iteration.
+    init =
+        if (isfile( info.fileName ))
+            rLast = readLastLine( info )
+            println( "Found ancient iteration: $rLast" )
+            kLast = getLastKey( iKeys, iRanges, rLast )
+            getNextKey( iKeys, iRanges, kLast )
+        else
+            init
+        end
+    function runAndSave(paras)
+        data = f( paras )
+        dict2File(keyForData(paras, data), data, info)
+    end
+    iterFromInit(runAndSave, iKeys, iRanges)
 end
 
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,37 +1,186 @@
 using DataCSV, Test
 
-if (isfile("xy.csv"))
-    rm("xy.csv")
+sample = ( x = 5, y = 3, z = 1.5 )
+info = CSVInfo( sample, "xy.csv" ; skeys = [:x, :y, :z])
+
+if (isfile(info.fileName))
+    rm(info.fileName)
 end
 
-sample = ( x = 5, y = 3 )
-info = CSVInfo( sample, "./xy.csv" )
-for  x in 1:10
-    for y in 1:10
-        matrix = rand( x % 3 + 1, y % 4 + 1 )
-        avg = sqrt( 0.5 * ( x ^ 2 + y ^ 2))
-        dict2File( ( x = x, y = y ), Dict( :matrix  => matrix, :avg => avg ), info )
-        print( " x = $x, y = $y stored         \r" )
-    end
+
+# for  x in 1:10
+#     for y in 1:10
+#         matrix = rand( x % 3 + 1, y % 4 + 1 )
+#         avg = sqrt( 0.5 * ( x ^ 2 + y ^ 2))
+#         dict2File( ( x = x, y = y ), Dict( :matrix  => matrix, :avg => avg ), info )
+#         print( " x = $x, y = $y stored         \r" )
+#     end
+# end
+
+# println()
+
+# @info " iterate selected rows from a csv file"
+# @test isfile("xy.csv")
+
+# rows = file2Rows( info )
+
+# # all matrices of size 3 x 4
+
+# mx34 = findRows( r -> r.x % 3 == 2 && r.y % 4 == 3, rows; iter = true )
+
+# sum34 = foldl( ( x, y) -> (x .+ y[ :matrix ]), mx34 ; init = 0)
+# display( sum34 )
+# println()
+
+# @info "get all keys"
+
+# oks = file2Keys( info; lazyList = true)
+
+# for x in 1:20
+#     for y in 1:10
+#         key = (x = x, y = y)
+#         if (keyExists( key, oks))
+#             println("skipping $key   \r")
+#             continue
+#         end
+#         matrix = rand( x % 3 + 1, y % 4 + 1 )
+#         avg = sqrt( 0.5 * ( x ^ 2 + y ^ 2))
+#         data = Dict(:matrix => matrix, :avg => avg)
+#         dict2File( key, data, info )
+#         println("Key $key computed and stored \r")
+#     end
+# end
+
+# rows2 = file2Rows(info)
+# sampleRow =   findRows( sample, rows2 )  
+
+# @test length(sampleRow) == 1
+
+# @info "Testing the very delicate headFirst function ..."
+# function orderless(a1) 
+#     if (isempty(a1)) false
+#     else
+#         x, y = a1[1]
+#         if x < y true
+#         elseif x == y
+#             orderless(a1[2:end])
+#         else
+#             false
+#         end
+#     end
+# end
+
+
+# function f(t) 
+#     ab = [(t[m], 6) for m in [:w, :x, :y, :z]]
+#     orderless(ab)
+# end
+
+
+# pointers = [:w, :x, :y, :z]
+# ranges = Dict(:w=>0:7,:x=>2:8, :y=>0:13, :z=>7:9)
+
+# out = headFirst(pointers, ranges, f)
+# println(out)
+
+# @test  out[:w][1] == out[:x][1] == out[:y][1] == 6 && out[:z][1] == 7
+
+SymbolRange = Dict{Symbol, Union{Function, <: AbstractArray}}
+@info "Manual check: Whether next is true"
+rgF = SymbolRange(
+    :w => 1:10,
+    :x => d -> d[ :w ] : 10,
+    :y => d -> 1 : (d[ :x ] - d[ :w ] + 1),
+    :z => d -> d[ :y ] : d[ :y ] + 2
+)
+
+rgT = SymbolRange(
+    :w => 8 : 10,
+    :x => 10:10,
+    :y => 1:1,
+    :z => 3:3
+)
+
+nxtRg , status = DataCSV.getNextKey([:w, :x, :y, :z], rgF, rgT)
+println( " Last data: $rgT" )
+println( " Next data; $nxtRg  \n which is $status" )
+
+
+@info "Check Advanced Iteration Method:"
+
+iKeys = [:x, :y, :z]
+
+iRanges1 = SymbolRange(
+    :x => 1:5,
+    :y => 1:10,
+    :z => -3.0 : 3.0 : 3.0
+)
+
+iRanges2 = SymbolRange(
+    :x => 1:8,
+    :y => (d) -> (d[:x] < 5) ? (1 : 10) : (1 : d[:x] + 10),
+    :z => -3.0 : 3.0 : 3.0
+)
+
+iRanges3 = SymbolRange(
+    :x => 1:12,
+    :y => (d) -> (d[:x] < 5) ? (1 : 10) : (1 : d[:x] + 10),
+    :z => -3.0 : 3.0 : 3.0
+)
+
+function getData(paras)
+    x = paras[ :x ]
+    y = paras[ :y ]
+    z = paras[ :z ]
+    matrix = rand( x % 3 + 1, y % 4 + 1 ) .+ z
+    avg = sqrt( 0.5 * ( x ^ 2 + y ^ 2))
+    data = Dict(:matrix => matrix, :avg => avg)
+    print(" :->", (x, y, z))
+    data
 end
 
+@info "First computation precession:"
+
+iterForward(getData, iKeys, iRanges1, info)
+
+# get last iteration method 1 ( slow ):
+checker = DataCSV.getRapidChecker(info)
+init = DataCSV.headFirst(iKeys, iRanges2, checker)
+
+# get last iteration method 2 ( directly ):
+rLast = DataCSV.readLastLine( info )
+iterLast = DataCSV.getLastKey( iKeys, iRanges2, rLast )
+iterInit = DataCSV.getNextKey( iKeys, iRanges2, iterLast )
+
 println()
+println("Last run interrupted  before: $init\n")
+println("So the computation resumes @: $iterInit\n")
 
-@info " iterate selected rows from a csv file"
-@test isfile("xy.csv")
+@test isequal(iterInit, init)
 
-rows = file2Rows( info )
 
-# all matrices of size 3 x 4
-
-mx34 = findRows( r -> r.x % 3 == 2 && r.y % 4 == 3, rows; iter = true )
-
-sum34 = foldl( ( x, y) -> (x .+ y[ :matrix ]), mx34 ; init = 0)
-display( sum34 )
 println()
+@info "Computer rebooted and preceeded"
 
-@info "select with map"
-@test size( sum34 ) == ( 3, 4 )
+checker = DataCSV.getRapidChecker(info)
+@test checker((x=5, y=10, z=0.0))
+@test ! checker((x=5, y=11, z=3.0))
+
+
+
+iterForward(getData, iKeys, iRanges2, info)
+
+println()
+iterFromLast( getData, iKeys, iRanges3, info )
+
+println()
+@info "Checking total lines:"
+rows = collect(file2Rows(info))
+@test length(rows) == 564
+
+
+println()
+@info "Select with map"
 
 # all matrices of size 2 x 3
 
@@ -43,83 +192,5 @@ display( sum23 )
 println()
 @test size(sum23) == (2, 3)
 
-@info "get all keys"
-
-oks = file2Keys( info; lazyList = true)
-
-for x in 1:20
-    for y in 1:10
-        key = (x = x, y = y)
-        if (keyExists( key, oks))
-            println("skipping $key   \r")
-            continue
-        end
-        matrix = rand( x % 3 + 1, y % 4 + 1 )
-        avg = sqrt( 0.5 * ( x ^ 2 + y ^ 2))
-        data = Dict(:matrix => matrix, :avg => avg)
-        dict2File( key, data, info )
-        println("Key $key computed and stored \r")
-    end
-end
-
-rows2 = file2Rows(info)
-sampleRow =   findRows( sample, rows2 )  
-
-@test length(sampleRow) == 1
-
-@info "Testing the very delicate headFirst function ..."
-function orderless(a1) 
-    if (isempty(a1)) false
-    else
-        x, y = a1[1]
-        if x < y true
-        elseif x == y
-            orderless(a1[2:end])
-        else
-            false
-        end
-    end
-end
-
-
-function f(t) 
-    ab = [(t[m], 6) for m in [:w, :x, :y, :z]]
-    orderless(ab)
-end
-
-
-pointers = [:w, :x, :y, :z]
-ranges = Dict(:w=>0:7,:x=>2:8, :y=>0:13, :z=>7:9)
-
-out = headFirst(pointers, ranges, f)
-println(out)
-
-@test  out[:w][1] == out[:x][1] == out[:y][1] == 6 && out[:z][1] == 7
-
-
-@info "Check Advanced Iteration Method:"
-
-iKeys = [:x, :y]
-iRanges = Dict(
-    :x => 1:23,
-    :y => (d) -> (d[:x] < 21) ? (1 : 10) : (1 : d[:x] - 20)
-)
-
-function getData(paras)
-    x = paras[ :x ]
-    y = paras[ :y ]
-    matrix = rand( x % 3 + 1, y % 4 + 1 )
-    avg = sqrt( 0.5 * ( x ^ 2 + y ^ 2))
-    data = Dict(:matrix => matrix, :avg => avg)
-    println("Smart Iteration at: x = $x, y = $y")
-    data
-end
-
-iterForward(getData, iKeys, iRanges, info)
-
-rows = collect(file2Rows(info))
-
-@test length(rows) == 206
-
-rm( "xy.csv" )
+rm( info.fileName )
 
